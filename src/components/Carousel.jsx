2869249ed0338879ea2520abb584771763d3a85f
Ricardo Vazquez <rickyrabv@gmail.com>
2024-04-19 23:08:26 +0200

    error page added

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,11 +4,11 @@ import { v4 as uuidv4 } from "uuid";
 export default function Carousel(props) {
   const { images } = props;
   return (
-    <div className="w-80 lg:w-full carousel rounded-box">
+    <div className="w-80 lg:w-5/6 carousel rounded-box">
       {images.map((image) => {
         return (
           <div className="carousel-item w-full" key={uuidv4()}>
-            <LazyLoadImage src={image}  className=""/>
+            <LazyLoadImage src={image} />
           </div>
         );
       })}
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,3 +1,19 @@
+import { Link } from "react-router-dom";
+
 export default function ErrorPage() {
-  return <h1>Error Page</h1>;
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">OOPS!</h1>
+          <p className="py-6">
+            My website is currently still under construction or the desired link
+            does not exist. Please check in again soon to see further
+            developments.
+          </p>
+          <Link to={"/"} className="btn btn-primary">Go Home</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
